9ececfb77b9de176792e56c3325ffd5a7ac3fda1
taylorvercillo <taylornvcasey@gmail.com>
2021-11-29 23:23:35 -0700

    All done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById("main"); 
+main.remove(); 
+
+let newHeader = document.createElement('h1'); 
+document.body.appendChild(newHeader); 
+newHeader.id = "victory"; 
+
+newHeader.textContent = "YOUR-NAME is the champion"; 
